88a3724aa9d0a491dc212f68606092426e9c64be
bryan <fanwang2@amd.com>
2025-04-09 16:30:24 +0800

    support both cuda and hip

--- setup.py
+++ setup.py
@@ -8,20 +8,25 @@
 #
 # For inquiries contact  george.drettakis@inria.fr
 #
-import torch
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension, ROCM_HOME
 import os
+import torch
 
 cxx_compiler_flags = []
 
 # Include this line immediately after the import statements
 TORCH_MAJOR = int(torch.__version__.split('.')[0])
 TORCH_MINOR = int(torch.__version__.split('.')[1])
-is_rocm_pytorch = False
+is_rocm = False
 if TORCH_MAJOR > 1 or (TORCH_MAJOR == 1 and TORCH_MINOR >= 5):
-  is_rocm_pytorch = True if ((torch.version.hip is not None) and (ROCM_HOME is not None)) else False
-print("is_rocm_pytorch = ", is_rocm_pytorch)
+  is_rocm = True if ((torch.version.hip is not None) and (ROCM_HOME is not None)) else False
+
+def get_sources():
+    if is_rocm:
+        return ["spatial.hip", "simple_knn.hip", "ext.cpp"]
+    else:
+        return ["spatial.cu", "simple_knn.cu", "ext.cpp"]
   
 if os.name == 'nt':
     cxx_compiler_flags.append("/wd4624")
@@ -31,14 +36,11 @@ setup(
     ext_modules=[
         CUDAExtension(
             name="simple_knn._C",
-            sources=[
-            "spatial.hip", 
-            "simple_knn.hip",
-            "ext.cpp"],
+            sources=get_sources(),
             extra_compile_args={"nvcc": [], "cxx": cxx_compiler_flags})
         ],
     cmdclass={
-        'build_ext': BuildExtension.with_options(use_hip=True)
+        'build_ext': BuildExtension
     },
     version='1.0.0'
 )
